11626c4ca5ac3500fea91c3ce3158eb7dea0562e
ElizAnto <elizarov@anton-olegovich.ru>
2025-03-24 16:05:38 +0400

    The Get Method

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -7,3 +7,8 @@ class TestCreatePlace():
     def test_create_new_place(self):
         print("Метод POST")
         result_post = GoogleMapsAPI.create_new_place()  # Вызов метода по созданию новой локации
+        check_post = result_post.json()
+        place_id = check_post.get("place_id")
+
+        print("Метод GET POST")
+        result_get = GoogleMapsAPI.get_new_place(place_id)  # Вызов метода для проверки новой локации
